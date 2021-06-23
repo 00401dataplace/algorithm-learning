e0db5ae1da131b96105e7ffc53c10349ccb48113
nemo00407 <nemo00407@gmail.com>
2021-06-24 00:03:09 +0800

    OJ

--- UVa_11549.cpp
+++ UVa_11549.cpp
@@ -3,29 +3,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _for(i, a, b) for(int i = (a); i < (b); ++i)
+#define _for(i, a, b) for(int i = (a); i < (b); i++)
 typedef long long LL;
 LL K, M;
+
 LL next(LL x) {
-    LL ans = x * x;
+    int ans = x * x;
     while (ans >= M) ans /= 10;
     return ans;
-}
+}// 利用上限值取出溢出的答案
+
 
 int main() {
     int T, n;
     scanf("%d", &T);
     while (T--) {
-        scanf("%d%lld", &n, &K);
+        scanf("%d", &n); // 要求的位數
+        scanf("%lld", &K); // 起始值
+        // 設定上限值
         M = 1;
         _for(i, 0, n) M *= 10;
-        LL ans = K, k1 = K, k2 = K;
         // Floyd判圈
+        LL ans = K, k1 = K, k2 = K;
         do{
             k1 = next(k1), ans = max(ans, k1);
             k2 = next(k2), ans = max(ans, k2);
             k2 = next(k2), ans = max(ans, k2);
         }while (k1 != k2);
         printf("%lld\n", ans);
+
     }
 }
